59483e75402059510e2c46a9060072969dce84cf
gsv80 <73550391+gsv80@users.noreply.github.com>
2021-09-16 09:16:50 +0300

    changes to resolve comments in pull request for homework-4

--- src/components/MenuHeader/NavBar/index.js
+++ src/components/MenuHeader/NavBar/index.js
@@ -2,13 +2,11 @@ import { useState } from 'react';
 import cn from 'classnames';
 import s from './style.module.css'
 
-function NavBar( {onOpenBurger} ) {
+function NavBar( {onOpenBurger, isActive} ) {
 
-    const [isActive, setActive]=useState(false);
+    
 
     const burgerOpen = () => {
-        console.log('####: open burger menu');
-        setActive(!isActive);
         onOpenBurger && onOpenBurger ();
     }
 
--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,15 +1,13 @@
 import { useState } from 'react';
-import Menu from '../Menu/index.js';
-import NavBar from '../NavBar/index.js';
+import Menu from './Menu/index.js';
+import NavBar from './NavBar/index.js';
 
 function MenuHeader({onOpenBurger}) {
 
     const [isActive, setActive] = useState(null);
     const burgerChanged = () =>{
-        console.log('####: <pushed burger button />');
         setActive (!isActive);
-        // console.log (state +" changedMenu");
-        // changedMenu(state );
+        
     }  
 
 
@@ -18,10 +16,11 @@ function MenuHeader({onOpenBurger}) {
             <Menu 
                 isActive ={isActive}
                 onOpenBurger = {onOpenBurger}
-
-            />
+                
+                />
             
             <NavBar 
+                isActive ={isActive}
                 onOpenBurger = {burgerChanged}
             
             />
